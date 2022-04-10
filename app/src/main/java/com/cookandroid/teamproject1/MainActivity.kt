c57e27f8c6da90c40216d3ce24f4716115f47cdf
mac <dohye@macui-MacBookAir.local>
2022-04-11 03:02:04 +0900

    commit

--- app/src/main/java/com/cookandroid/teamproject1/MainActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/MainActivity.kt
@@ -2,11 +2,15 @@ package com.cookandroid.teamproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cookandroid.teamproject1.databinding.ActivityMainBinding
+import com.cookandroid.teamproject1.databinding.ActivitySignupctfBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_title)
+        setContentView(R.layout.sign_uping)
+
+
     }
 }
