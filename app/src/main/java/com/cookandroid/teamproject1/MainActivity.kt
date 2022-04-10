7bff82641f25695c8f8fddefaa759c4e5281975f
mac <dohye@macui-MacBookAir.local>
2022-04-11 03:33:40 +0900

    seongsik

--- app/src/main/java/com/cookandroid/teamproject1/MainActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/MainActivity.kt
@@ -2,8 +2,6 @@ package com.cookandroid.teamproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cookandroid.teamproject1.databinding.ActivityMainBinding
-import com.cookandroid.teamproject1.databinding.ActivitySignupctfBinding
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
@@ -15,15 +15,11 @@ class SignUpingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_uping)
 
-
         setSupportActionBar(signuping_toolbar)//커스텀한 툴바 액션바로 use
         supportActionBar!!.setDisplayShowTitleEnabled(false)
         //액션바에 표시되는 제목의 표시유무 결정 false로 해야 툴바 이름 화면에 보임
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.back_btn)
-
-
-
 
     }
 
