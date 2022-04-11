97c42f88f9543ca54170eb1723c981e03b9a5de6
LeeChoongHwan <choo901@naver.com>
2022-04-11 21:41:50 +0900

    0411 commit

--- app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
@@ -19,29 +19,10 @@ class SignUpingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_uping)
 
-
-
-        setSupportActionBar(signuping_toolbar)//커스텀한 툴바 액션바로 use
-        supportActionBar!!.setDisplayShowTitleEnabled(false)
-        //액션바에 표시되는 제목의 표시유무 결정 false로 해야 툴바 이름 화면에 보임
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
-        supportActionBar!!.setHomeAsUpIndicator(R.drawable.back_btn)
-
+        //임시로 설정(수정 요망)
         button5.setOnClickListener {
             startActivity(Intent(this,CreateAccountActivity::class.java))
         }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.sign_uping_menu, menu)       // sign uping menu 메뉴를 toolbar 메뉴 버튼으로 설정
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
-    }
-
-
 
 }
