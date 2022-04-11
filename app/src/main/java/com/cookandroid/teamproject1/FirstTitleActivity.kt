2d651381e437a205c25bcb87035d6a2797915e27
LeeChoongHwan <choo901@naver.com>
2022-04-11 17:36:11 +0900

    page connection

--- app/src/main/java/com/cookandroid/teamproject1/FirstTitleActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/FirstTitleActivity.kt
@@ -1,12 +1,28 @@
 package com.cookandroid.teamproject1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cookandroid.teamproject1.databinding.CreateAccountBinding
+import com.cookandroid.teamproject1.databinding.FirstTitleBinding
+import com.cookandroid.teamproject1.databinding.SignInBinding
 
 class FirstTitleActivity : AppCompatActivity() {
 
+    lateinit var binding: FirstTitleBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_title)
+        binding = FirstTitleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textSignup.setOnClickListener {
+            startActivity(Intent(this,SignUpingActivity::class.java))
+        }
+
+        binding.textSignin.setOnClickListener {
+            startActivity(Intent(this,SigninActivity::class.java))
+        }
+
+
     }
 }
--- app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
@@ -1,12 +1,16 @@
 package com.cookandroid.teamproject1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.ActionBar
+import com.cookandroid.teamproject1.databinding.SignInBinding
+import com.cookandroid.teamproject1.databinding.SignUpBinding
 import kotlinx.android.synthetic.main.sign_uping.*
 
 class SignUpingActivity : AppCompatActivity() {
@@ -16,17 +20,17 @@ class SignUpingActivity : AppCompatActivity() {
         setContentView(R.layout.sign_uping)
 
 
+
         setSupportActionBar(signuping_toolbar)//커스텀한 툴바 액션바로 use
         supportActionBar!!.setDisplayShowTitleEnabled(false)
         //액션바에 표시되는 제목의 표시유무 결정 false로 해야 툴바 이름 화면에 보임
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.back_btn)
 
-
-
-
+        button5.setOnClickListener {
+            startActivity(Intent(this,CreateAccountActivity::class.java))
+        }
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.sign_uping_menu, menu)       // sign uping menu 메뉴를 toolbar 메뉴 버튼으로 설정
