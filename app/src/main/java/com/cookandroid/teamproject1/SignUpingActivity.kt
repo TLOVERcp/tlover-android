b20a2319eb8441e52100d73a32c57efbe824a67b
mac <dohye@macui-MacBookAir.local>
2022-04-13 22:07:04 +0900

    selectview

--- app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/SignUpingActivity.kt
@@ -3,14 +3,7 @@ package com.cookandroid.teamproject1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.Menu
 import android.view.View
-import androidx.appcompat.app.ActionBar
-import com.cookandroid.teamproject1.databinding.SignInBinding
-import com.cookandroid.teamproject1.databinding.SignUpBinding
-import kotlinx.android.synthetic.main.sign_uping.*
 import com.cookandroid.teamproject1.databinding.SignUpingBinding
 
 class SignUpingActivity : AppCompatActivity() {
@@ -32,7 +25,7 @@ class SignUpingActivity : AppCompatActivity() {
             sbinding.btnTransport.visibility = View.VISIBLE
             sbinding.signTextCtf.visibility = View.VISIBLE
             sbinding.signUpMsgctf.visibility = View.VISIBLE
-            sbinding.signUpingCtfnumber.visibility = View.VISIBLE
+            sbinding.signupingCtfnumber.visibility = View.VISIBLE
             sbinding.textTimer.visibility = View.VISIBLE
         }
     }
