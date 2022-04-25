b47f6e1a5ad0628a90dc177963d41faa85be0e6c
sgsk88 <sgsk88@nate.com>
2022-04-25 22:24:32 +0900

    dohye merge

--- app/src/main/java/com/cookandroid/teamproject1/id/view/IntroActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/id/view/IntroActivity.kt
@@ -1,10 +1,12 @@
-package com.cookandroid.teamproject1
+package com.cookandroid.teamproject1.id.view
 
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import android.os.Bundle
+import com.cookandroid.teamproject1.FirstTitleActivity
+import com.cookandroid.teamproject1.R
 
 class IntroActivity : AppCompatActivity() {
 
@@ -15,7 +17,7 @@ class IntroActivity : AppCompatActivity() {
 
       var handler=Handler()
         handler.postDelayed({
-            var intent=Intent(this,FirstTitleActivity::class.java)
+            var intent=Intent(this, FirstTitleActivity::class.java)
             startActivity(intent)// 다음 화면으로 넘어가기
         },2000)//2초뒤 실행
     }
--- app/src/main/java/com/cookandroid/teamproject1/id/view/LoginSuccessActivity.kt
+++ app/src/main/java/com/cookandroid/teamproject1/id/view/LoginSuccessActivity.kt
@@ -1,10 +1,11 @@
-package com.cookandroid.teamproject1
+package com.cookandroid.teamproject1.id.view
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import android.os.Bundle
 import android.os.Looper
+import com.cookandroid.teamproject1.HomeActivity
 import com.cookandroid.teamproject1.R
 
 class LoginSuccessActivity : AppCompatActivity() {
@@ -17,7 +18,7 @@ class LoginSuccessActivity : AppCompatActivity() {
       var handler = Handler(Looper.getMainLooper())
 
         handler.postDelayed({
-            startActivity(Intent(this,HomeActivity::class.java))
+            startActivity(Intent(this, HomeActivity::class.java))
         },1000)
     }
 
