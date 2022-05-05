a978df29030bf8e79632fa88d9004ac417cc88bc
LeeChoongHwan <choo901@naver.com>
2022-05-05 22:13:52 +0900

    0505 push ver2

--- app/src/main/java/com/cookandroid/teamproject1/home/model/HomeHotRecommendDataModel.kt
+++ app/src/main/java/com/cookandroid/teamproject1/home/model/HomeHotRecommendDataModel.kt
@@ -1,5 +1,8 @@
 package com.cookandroid.teamproject1.home.model
 
+/**
+ *  home 두번째 리사이클러뷰 데이터모델
+ */
 data class HomeHotRecommendDataModel(
     var title: String?="",
     var image: Int? = null,
--- app/src/main/java/com/cookandroid/teamproject1/home/view/adapter/HomeHotRecommendRVAdapter.kt
+++ app/src/main/java/com/cookandroid/teamproject1/home/view/adapter/HomeHotRecommendRVAdapter.kt
@@ -8,7 +8,10 @@ import com.cookandroid.teamproject1.databinding.ItemHomeRandomSecondBinding
 import com.cookandroid.teamproject1.home.model.HomeDataModel
 import com.cookandroid.teamproject1.home.model.HomeHotRecommendDataModel
 
-
+/**
+ * 홈 화면에서 나오는 두 번째 리사이클러뷰
+ * 다이어리 목록 조회
+ */
 class HomeHotRecommendRVAdapter(private val recommendList: ArrayList<HomeHotRecommendDataModel>) :RecyclerView.Adapter<HomeHotRecommendRVAdapter.Holder>(){
 
     class Holder(val binding : ItemHomeRandomSecondBinding) : RecyclerView.ViewHolder(binding.root){
